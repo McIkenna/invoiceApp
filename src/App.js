7745ac8dbbbe21d5a469f9ac71ef0f89916c1c70
Ikenna <ifekaonwuikenna@gmail.com>
2022-02-16 15:56:59 -0500

    first-commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,72 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react'
+import { dummy } from './DummData'
+import {Table, Button} from 'reactstrap'
+import {faImage, faThumbsUp, faThumbsDown, faMoneyCheckAlt, faSearchDollar} from '@fortawesome/free-solid-svg-icons'
+import {FontAwesomeIcon} from '@fortawesome/react-fontawesome'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default class App extends Component {
+  state = {
+    isLoading: false,
+    invoices : dummy
+  }
+
+
+removeItem = (id)=> {
+  let updatedInvoices = [...this.state.invoices].filter (i => i.id !== id)
+  this.setState({invoices : updatedInvoices})
+}
+  render() {
+    const isLoading = this.state.isLoading;
+    const allInvoices = this.state.invoices;
+
+    let invoices = allInvoices.map(invoice =>
+      <tr key={invoice.id}>
+        <td>{invoice.vendor}</td>
+        <td>{invoice.amount}</td>
+        <td>{invoice.date}</td>
+        <td>{invoice.date}</td>
+        <td><Button className='btn btn-lg btn-success' onClick={() => this.removeItem(invoice.id)}><FontAwesomeIcon icon={faThumbsUp}/>OK</Button></td>
+        <td><Button className='btn btn-lg btn-danger' onClick={() => this.removeItem(invoice.id)}><FontAwesomeIcon icon={faThumbsDown}/>NOT OK</Button></td>
+        <td><Button className='btn btn-lg btn-info' onClick={() => this.removeItem(invoice.id)}><FontAwesomeIcon icon={faMoneyCheckAlt}/>50%</Button></td>
+        <td><Button className='btn btn-lg btn-warning' onClick={() => this.removeItem(invoice.id)}><FontAwesomeIcon icon={faSearchDollar}/>??</Button></td>
+        <td><Button className='btn btn-lg btn-info' onClick={() => this.removeItem(invoice.id)}><FontAwesomeIcon icon={faImage}/></Button>Image</td>
+      </tr>)
+    if(isLoading){
+      return(<div>...isLoading</div>);
+    }
+    return (
+      <div className='constainer border border-primary rounded center'>
+       
+          <div className='row'>
+            <div className='col-12'>
+            <h4>The Vendor List</h4>
+          </div>
+          </div>
+
+          <div className='row'>
+            <div className='.col-xs-12 center text-center'>
+              <Table dark responsive striped bordered hover>
+                <thead>
+                  <tr>
+                  <th>Vendor</th>
+                  <th>Amount</th>
+                  <th>Invoice Number</th>
+                  <th>Date</th>
+                  <th colSpan ='4'>Actions</th>
+                  <th>Image</th>
+                  </tr>
+                </thead>
+                <tbody>
+                  {this.state.invoices.length === 0 ? <td colSpan="9">all caught up!</td> : 
+                  invoices}
+                </tbody>
+              </Table>
+
+            </div>
+
+          </div>
+      </div>
+    )
+  }
 }
 
-export default App;
---
+++ src/DummData.js
@@ -0,0 +1,30 @@
+export const dummy = [
+    {
+      "id": "100",
+      "vendor": "Hankok",
+      "invoice": "2345",
+      "amount": "$1800",
+      "date": "08/21/2019"
+    },
+    {
+      "id": "101",
+      "vendor": "Books",
+      "invoice": "1289",
+      "amount": "$20000",
+      "date": "07/20/2021"
+    },
+    {
+      "id": "102",
+      "vendor": "Computer",
+      "invoice": "2990",
+      "amount": "$1,234,800",
+      "date": "04/09/2022"
+    },
+    {
+      "id": "103",
+      "vendor": "Table Chair",
+      "invoice": "3409",
+      "amount": "$1,800,500",
+      "date": "02/04/2020"
+    },
+  ]
